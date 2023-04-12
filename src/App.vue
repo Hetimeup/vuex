9c2bbf599f1a360c04c12dbfa7aaf9cb4b707d21
Harry <2439962483@qq.com>
2023-04-12 19:01:29 +0800

    初始化计数器项目的基本结构

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Addition></Addition>
+
+    <p>------------------</p>
+
+    <Subtraction></Subtraction>
+  </div>
+</template>
+
+<script>
+import Addition from './components/Addition.vue'
+import Subtraction from './components/Subtraction.vue'
+export default {
+  components: {
+    Addition,
+    Subtraction
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Addition.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>当前 Count 值为：</h3>
+    <button>+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
