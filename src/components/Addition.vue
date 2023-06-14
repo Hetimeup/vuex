d40210e95a3006b9d39d24f5e1a4ca4255ac4b1d
Harry <2439962483@qq.com>
2023-06-14 10:57:04 +0800

    第一次提交

--- src/components/Addition.vue
+++ src/components/Addition.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <h3>当前 Count 值为：</h3>
-    <button>+1</button>
+    <!-- <h3>当前 Count 值为：{{ $store.state.count }}</h3> -->
+    <h3>{{ $store.getters.showNum }}</h3>
+    <button @click="btnhandler1">+1</button>
+    <button @click="btnhandler2">+N</button>
+    <button @click="btnhandler3">+1 Async</button>
+    <button @click="btnhandler4">+N Async</button>
   </div>
 </template>
 
@@ -9,6 +13,20 @@
 export default {
   data() {
     return {}
+  },
+  methods: {
+    btnhandler1() {
+      this.$store.commit('add')
+    },
+    btnhandler2() {
+      this.$store.commit('addN', 3)
+    },
+    btnhandler3() {
+      this.$store.dispatch('addAsync')
+    },
+    btnhandler4() {
+      this.$store.dispatch('addNAsync', 5)
+    }
   }
 }
 </script>
